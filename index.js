835db2c7245f730b9ff90d35d7943225fd0f456d
tunnckoCore <mameto_100@mail.bg>
2016-10-24 15:13:43 +0300

    chore(*): initial

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * tunnckoCore/minibase <https://github.com/tunnckoCore/tunnckoCore/minibase>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function MiniBase () {
+  // stuff
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+/*!
+ * tunnckoCore/minibase <https://github.com/tunnckoCore/tunnckoCore/minibase>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
